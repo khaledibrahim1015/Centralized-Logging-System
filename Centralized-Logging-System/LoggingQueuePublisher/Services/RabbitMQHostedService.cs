24a38285aea5cf242219039f8d5ef3c54c032dd6
Khaled.Ibrahim <Khaled.Ibrahim@IT-KALEDIBRAIHM>
2024-07-30 09:44:01 +0300

    add RabbitmqService and RabbitMQHostedService for LoggingQueuePublisher

---
+++ Centralized-Logging-System/LoggingQueuePublisher/Services/RabbitMQHostedService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingQueuePublisher.Services
+{
+    public class RabbitMQHostedService : IHostedService
+    {
+        private readonly RabbitMQService _rabbitMqService;
+        private readonly ILogger<RabbitMQHostedService> _logger;
+
+        public RabbitMQHostedService(RabbitMQService rabbitMqService, ILogger<RabbitMQHostedService> logger)
+        {
+            _rabbitMqService = rabbitMqService;
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _rabbitMqService.CreateQueue();
+            _logger.LogInformation("Create Queue Successfully !");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Centralized-Logging-System/LoggingQueuePublisher/Services/RabbitMQService.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingQueuePublisher.Services
+{
+    public class RabbitMQService
+    {
+        private readonly string _hostname;
+        private readonly string _queueName;
+        private readonly int _port;
+        private readonly string _username ;
+        private readonly string _password ;
+
+        public RabbitMQService(string hostname, string queueName , int port ,string username , string  password  )
+        {
+            _hostname = hostname;
+            _queueName = queueName;
+            _port = port;
+            _username = username;
+            _password = password;
+        }
+
+        public void CreateQueue()
+        {
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.UserName = _username;
+            factory.Password = _password;
+
+            var endpoints = new System.Collections.Generic.List<AmqpTcpEndpoint> {
+                            new AmqpTcpEndpoint(_hostname,_port),
+                          };
+            var conn = factory.CreateConnection(endpoints);
+            var ch = conn.CreateModel();
+            ch.QueueDeclare(queue: _queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+    }
+}
