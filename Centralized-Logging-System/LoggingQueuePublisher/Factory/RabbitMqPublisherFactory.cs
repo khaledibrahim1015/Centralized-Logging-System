808fe597acaff30ed39cd961275a058c02a236ee
Khaled.Ibrahim <Khaled.Ibrahim@IT-KALEDIBRAIHM>
2024-07-30 09:41:42 +0300

    add RabbitMqPublisherFactory for LoggingQueuePublisher

---
+++ Centralized-Logging-System/LoggingQueuePublisher/Factory/RabbitMqPublisherFactory.cs
@@ -0,0 +1,32 @@
+﻿using LoggingQueuePublisher.Configuration;
+using LoggingQueuePublisher.Services;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingQueuePublisher.Factory
+{
+    public class RabbitMqPublisherFactory
+    {
+
+        internal readonly RabbitMqClientConfig _config;
+        public RabbitMqPublisherFactory(IOptions<RabbitMqClientConfig> config)
+        {
+            _config = config.Value;
+        }
+
+        public RabbitMQService CreateRabbitMQService()
+        {
+            return new RabbitMQService(_config.HostName,
+                                        _config.RouteKey
+                                        , _config.Port
+                                          , _config.UserName
+                                            , _config.Password);
+
+        }
+
+    }
+}
