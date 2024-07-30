0b9279667124ff1221fe4f0f4ed868fee3b54e48
Khaled.Ibrahim <Khaled.Ibrahim@IT-KALEDIBRAIHM>
2024-07-30 09:41:05 +0300

    add ServiceCollectionExtensions for LoggingQueuePublisher

---
+++ Centralized-Logging-System/LoggingQueuePublisher/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using LoggingQueuePublisher.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using LoggingQueuePublisher.Configuration;
+using LoggingQueuePublisher.Factory;
+
+namespace LoggingQueuePublisher.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+
+        public static IServiceCollection AddRabbitMQ(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.Configure<RabbitMqClientConfig>(option => configuration.GetSection("RabbitMqClientConfig").Bind(option));
+            services.AddSingleton<RabbitMqPublisherFactory>();
+
+
+            services.AddSingleton(serviceProvider =>
+            {
+                var factory = serviceProvider.GetRequiredService<RabbitMqPublisherFactory>();
+                return factory.CreateRabbitMQService();
+            });
+
+            services.AddHostedService<RabbitMQHostedService>();
+
+            return services;
+        }
+
+    }
+}
