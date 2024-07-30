74fd723576e03094aeb64e4b935ee70e2351a79c
Khaled.Ibrahim <Khaled.Ibrahim@IT-KALEDIBRAIHM>
2024-07-30 09:53:48 +0300

    add ServiceCollectionExtensions to Register Service in DI Container for ConsumerWorker

---
+++ Centralized-Logging-System/ConsumerWorker/Helper/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using ConsumerWorker.Factory;
+using ConsumerWorker.RabbitMQService;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddRabbitMQConsumers(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<RabbitMQSettings>(configuration.GetSection("RabbitMQSettings"));
+        services.AddSingleton<RabbitMQConsumerFactory>();
+
+        services.AddSingleton(serviceProvider =>
+        {
+            var factory = serviceProvider.GetRequiredService<RabbitMQConsumerFactory>();
+            var consumers = factory.GetConsumers();
+            return consumers;
+        });
+
+        // Register each consumer as a singleton service
+        services.AddSingleton<IEnumerable<RabbitMQConsumer>>(serviceProvider =>
+        {
+            var factory = serviceProvider.GetRequiredService<RabbitMQConsumerFactory>();
+            return factory.GetConsumers();
+        });
+
+        return services;
+    }
+}
